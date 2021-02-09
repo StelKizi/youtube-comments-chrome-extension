f772c329223f7135adf1e9f37552068872c8bcc4
Stelkizi <stelkiziridou@gmail.com>
2021-02-09 19:12:51 +0100

    added floating comment functionality, not working properly

--- foreground.js
+++ foreground.js
@@ -1,7 +1,7 @@
 const queryParams = new URLSearchParams(location.search);
 let comments = [];
 let queuedComments = [];
-currentVideoTime = 0.0;
+let currentVideoTime = 0.0;
 let interval = null;
 
 chrome.runtime.sendMessage({
@@ -14,6 +14,57 @@ document.addEventListener('keyup', e => {
     clearInterval(interval);
   }
 });
+
+const displayComment = (author, comment) => {
+  let youtubeVideo = document
+    .querySelector('.video-stream')
+    .getBoundingClientRect();
+  let commentToShow = document.createElement('div');
+
+  commentToShow.classList.add('popup');
+
+  /* Motion variables */
+  const center = {
+    x: youtubeVideo.left + youtubeVideo.width / 5,
+    y: youtubeVideo.top + youtubeVideo.height,
+  };
+
+  let newX = 0.0;
+  let newY = 0.0;
+  let motionSway = 0.01;
+  let naturalSway = 0.1;
+  let animatedValue = 0.0;
+
+  commentToShow.style.left = `${center.x}px`;
+  commentToShow.style.top = `${center.y}px`;
+  commentToShow.innerText = `${author} ${comment}`;
+  document.querySelector('body').append(commentToShow);
+
+  /* Opacity variables */
+  let alteredOpacity = 0.0;
+  let opacitySpeed = 0.007;
+
+  const animate = () => {
+    newY = center.y - animatedValue;
+    newX = center.x + 60.0 * Math.sin(motionSway * animatedValue) + naturalSway;
+
+    commentToShow.style.top = `${newY}px`;
+    commentToShow.style.top = `${newX}px`;
+
+    opacity = Math.sin(opacitySpeed * animatedValue);
+
+    commentToShow.style.opacity = alteredOpacity;
+
+    animatedValue = animatedValue + 1;
+
+    if (commentToShow.style.opacity < 1) {
+      return;
+    } else {
+      requestAnimationFrame(animate);
+    }
+  };
+  animate();
+};
 
 currentVideoTime = document.querySelector('.video-stream').currentTime;
 
@@ -37,6 +88,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       queuedComments.forEach(queuedItem => {
         if (currentVideoTime == queuedItem.timestamp) {
           console.log(queuedItem);
+          displayComment(queuedItem.author, queuedItem.comment);
           queuedComments.splice(queuedComments.indexOf(queuedItem), 1);
         }
       });
